e587471ed9d094444aab30a1d0a2271e37a25031
Moumy Ndiaye <NdiayeMoumy@outlook.com>
2023-01-26 11:32:06 -0500

    s1c2 ajout de add_action pour enfiler style.css dans functions.php

--- functions.php
+++ functions.php
@@ -3,8 +3,11 @@
 * L'ensemble des fonctions du thème 
 */
 
-wp_enqueue_style('4w4-gr2-principal', // identificateur
-                get_template_directory_uri() . '/style.css', // adresse url de style.css
-                array(), // définir les dépendances
-                filemtime(get_template_directory() . '/style.css'), // le calcul de la version du fichier css
-                false); // média
+function enfiler_css() {
+    wp_enqueue_style('4w4-gr1-principal', // identificateur
+                    get_template_directory_uri() . '/style.css', // adresse url de style.css
+                    array(), // définir les dépendances
+                    filemtime(get_template_directory() . '/style.css'), // le calcul de la version du fichier css
+                    false); // média
+}
+add_action( 'wp_enqueue_scripts', 'enfiler_css' );
