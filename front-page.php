12832ca2e0f0388628b84a698b7a491dada995cd
Moumy Ndiaye <NdiayeMoumy@outlook.com>
2023-02-03 11:20:54 -0500

    lab2 ajout de scss sur la front-page

--- front-page.php
+++ front-page.php
@@ -11,7 +11,6 @@
             <h1>Bienvenue sur 4W4</h1>
             <?php if(have_posts()): 
                 while(have_posts()): the_post(); ?>
-
                 <article>
                     <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
                     <?php the_excerpt(); ?>
