33e591934daae18d3c21dd20e9f7b3d034a72e7a
Moumy Ndiaye <NdiayeMoumy@outlook.com>
2023-02-28 09:14:39 -0500

    s6c1 page  404

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 */ 
 ?>
     <?php get_header(); ?>    
-        <main>
+        <main class="erreur__main">
             <!-- <pre>404.php.php</pre> -->
             <h1>Erreur 404</h1>
         </main>
