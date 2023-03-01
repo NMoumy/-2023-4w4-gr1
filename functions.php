82d2fde7691e179377eb8e9b0367e30b13eeacdd
Moumy Ndiaye <NdiayeMoumy@outlook.com>
2023-03-01 13:09:16 -0500

    tp1 ajout de les variables couleurs

--- functions.php
+++ functions.php
@@ -25,8 +25,8 @@ add_action( 'after_setup_theme', 'enregistre_menus', 0 );
 /* ------------------------------------------ add_theme_support */
 add_theme_support( 'title-tag' );
 add_theme_support( 'custom-logo', array(
-    'height' => 150,
-    'width'  => 150,
+    'height' => 500,
+    'width'  => 500,
 ) );
 
 /**
