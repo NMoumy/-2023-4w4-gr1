dd1f622811ad36ee37723c1bfacec116c895101d
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-03-17 21:53:46 -0400

    Tp1 ajout de hover sur des éléments

--- template-parts/categorie-cours.php
+++ template-parts/categorie-cours.php
@@ -6,16 +6,17 @@
   */
   $titre = get_the_title();
   $sigle = substr($titre, 0, 7);
-  $titre = get_the_title();
   $aTrouver = '(';
   $postion = strpos(substr($titre, 7), $aTrouver);
   $titre_long = substr($titre, 7, $postion);
-  $duree = "45";
+  $duree = "90";
 ?>
 
 <article class="blocflex__article">
-    <h5><a href="<?php the_permalink(); ?>"><?= $sigle; ?></a></h5>
+  <a href="<?php the_permalink(); ?>">
+    <h5><?= $sigle; ?></h5>
     <h6><?= $titre_long; ?></h6>
     <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
     <h6><?= $duree; ?>h</h6>
+  </a>
 </article>
