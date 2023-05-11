6b04402f6a1a91d0c590416ebdbba8385f1639d2
Moumy Ndiaye <NdiayeMoumy@outlook.com>
2023-05-11 11:19:09 -0400

    Tp2 changement du style de la barre de recherche

--- searchform.php
+++ searchform.php
@@ -1,17 +1,6 @@
 <?php
 /* Formulaire de recherche personnalisée */
 ?>
-<!-- <form class="recherche" role="search" method="get"  action="<?php echo esc_url( home_url( '/' ) ); ?>">
-
-  <input type="checkbox" class="check" id="checked">
-  <label class="barre__recherche">
-    <input class="recherche__input" type="search" class="search-field" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s" />
-  </label>
-  <label class="btn__recherche" for="checked">
-    <span class="recherche__icone"><i class="fa fa-search"></i></span>
-  </label>
-
-</form> -->
 <form class="recherche" role="search" method="get"  action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <label class="barre__recherche">
     <input class="recherche__input" type="search" class="search-field" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s" />
