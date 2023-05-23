7f80e3d819506bff2f804f05b8c38ac7bd08b202
Moumy Ndiaye <NdiayeMoumy@outlook.com>
2023-05-23 09:14:59 -0400

    EF ajout de template-atelier.php

---
+++ template-atelier.php
@@ -0,0 +1,19 @@
+<?php
+  /**
+   * template name: atelier
+   */
+?>
+
+<?php get_header(); ?>
+  <main class="site__main page__evt">
+    <?php
+    if ( have_posts() ) : the_post(); ?>
+      <h1><?= get_the_title(); ?></h1>
+      <?php the_post_thumbnail('medium') ?>
+      <?php the_content();?>
+      <p>L'adresse de l'evénement: <?php the_field('adresse'); ?></p>
+      <p>La date et l'heure de l'événement: <?php the_field('date_et_heure_de_levenement'); ?></p>     
+    <?php endif;?>
+  </main><!-- #main -->
+<?php
+get_footer();
